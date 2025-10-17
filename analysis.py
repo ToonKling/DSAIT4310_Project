5ca6789c7a109dae67ce19a6254705e254057841
Toon Kling <t.kling@student.tudelft.nl>
2025-10-17 17:45:29 +0200

    feat: Add plot of vulnerability probability density function

--- analysis.py
+++ analysis.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import duckdb
+import matplotlib.pyplot as plt
 
 airports = pd.read_csv('airports.csv')
 airports = airports[airports['Country'] == 'United States']
 airports = airports[airports['IATA'].str.len() == 3]
 airlines = pd.read_csv('airlines.csv')
 routes = pd.read_csv('routes.csv')
-nr_rows = 100000 # Total dataset size is 5559465
+nr_rows = 1000000 # Total dataset size is 5559465
 delays = duckdb.query(f'SELECT * FROM "{'./delays.parquet'}" LIMIT {nr_rows};').df()
 
 # Filter out airports for which we have no flight information
 # Given 100000 rows, this leaves us with 60 airports, less than the 349 they have :(
 airports = airports[airports['IATA'].isin(delays['ORIGIN'])]
 
-for _, row in airports.iterrows():
+for i, row in airports.iterrows():
     airport_code = row['IATA']
     involving_airport = delays[(delays['ORIGIN'] == airport_code) | (delays['DEST'] == airport_code)].copy()
     delays_involving_airport = involving_airport[involving_airport['CARRIER_DELAY'] > 0]
@@ -29,14 +30,28 @@ for _, row in airports.iterrows():
     involving_airport = involving_airport[["DEP_SCH", "DEP_ACT", "ARR_SCH", "ARR_ACT"]]
     involving_airport = involving_airport.dropna()
 
-    # TODO: Exclude hours 0-6 for each day, see paper
     events = involving_airport.melt(value_name="timestamp", var_name="event_type")
+    events = events[events["timestamp"].dt.hour > 6] # Per paper we don't consider flights between 0 and 6 hours
     counts = events.groupby(["timestamp", "event_type"]).size().reset_index(name="total")
     pivoted = counts.pivot(index="timestamp", columns="event_type", values="total").fillna(0).astype(int)
     pivoted['PLANNED_CAP'] = pivoted['DEP_SCH'] + pivoted['ARR_SCH']
     pivoted['ACTUAL_CAP']  = pivoted['DEP_ACT'] + pivoted['ARR_ACT']
-    pivoted['VULN']        = pivoted['ACTUAL_CAP'] > 0.9 * pivoted['PLANNED_CAP'] # Assume alpha = 0.9
+    pivoted['VULN']        = pivoted['ACTUAL_CAP'] > pivoted['PLANNED_CAP'] / 0.9 # Assume alpha = 0.9
     vulnerability = pivoted['VULN'].mean()
+    airports.loc[i, 'VULN'] = vulnerability
 
+    print(f'pivoted for {airport_code} is \n{pivoted}\n')
     print(f'Vulnerability for {airport_code} is {vulnerability:.2f}\n')
+
+print(f'\n{airports}\n')
+
+plt.figure(figsize=(8, 5))
+
+plt.hist(airports["VULN"], bins=20, density=True, alpha=0.7, color='blue', edgecolor='black')
+
+plt.xlabel("Vulnerability")
+plt.xlim(0, 1)
+plt.ylabel("Probability Density")
+plt.title("Reproduction of Fig 1")
+plt.show()
 
